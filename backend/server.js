4cad5043dbe5a8d297067c9005c844b2c55fe5bb
Rajeshwaran <rajesh9585rajesh@gmail.com>
2025-06-17 13:03:23 +0530

    all changes done

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,8 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.error("MongoDB connection error:", err));
-//
-// Import student routes and use them
 
+// Import student routes and use them
 // const studentsRouter = require("./routes/students"); // Add this line to import the student routes
 // app.use("/api/students", studentsRouter);  // Add this to use the student routes at /api/students
 
